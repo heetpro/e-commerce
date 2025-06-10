3a88b5812224cb874f19c7b3d7cf8405417e1e8f
virtualheet <heetvavadiya456@gmail.com>
2025-06-10 13:02:11 +0530

    Add API routes for authentication, products, and orders. Implement health check endpoint and 404 error handler. Enhance request parsing with JSON and URL-encoded body support.

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import cors from "cors";
 import rateLimit from "express-rate-limit";
 import { config } from "./config/config";
 import morgan from "morgan";
+import authRoutes from "./routes/auth";
+import productsRoutes from "./routes/products";
+import ordersRoutes from "./routes/orders";
 
  const app = express()
 
@@ -24,3 +27,27 @@ import morgan from "morgan";
  if(config.nodeEnv === 'development'){
     app.use(morgan('dev'))
  }
+
+ app.use(express.json({limit: '10mb'}));
+ app.use(express.urlencoded({extended: true}));
+
+ //routes
+ app.use('/api/auth', authRoutes)
+ app.use('/api/products', productsRoutes)
+ app.use('/api/orders', ordersRoutes)
+
+
+ app.get('/health', (req,res) => {
+    res.json({
+        status: true,
+        message: 'API is running',
+        timestamp: new Date().toISOString()
+    })
+ })
+//404 handler
+ app.use('*', (req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found'
+    });
+});
